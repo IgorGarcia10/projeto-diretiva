5283d8083e850fe451bc837be351455aaadd019c
Igor <alexandre.igor64@gmail.com>
2021-04-13 20:34:59 -0300

    Exercicio Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,15 +8,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Diretiva';
 
+
   contador = 0;
 
   contaId(contador){
     return contador = contador+1;
   }
   
+
   pessoas = [
-    { id: this.contador ,nome: "José", idade: 18, cidade: "São Paulo", estado: "São Paulo"},
-    { id: this.contador, nome: "Maria", idade: 23, cidade: "Rio Janeiro", estado: "Rio de janeiro" }
+    { nome: "José", idade: 18, cidade: "São Paulo", estado: "São Paulo"},
+    { nome: "Maria", idade: 23, cidade: "Rio Janeiro", estado: "Rio de janeiro" }
   ];
 
   textoBotao = "Esconder";
@@ -26,8 +28,8 @@ export class AppComponent {
     this.esconder = !this.esconder;
   }
 
-  adicionar(id,nome, idade, cidade, estado) {
-    this.pessoas = [{ id: id, nome: nome, idade: idade, cidade: cidade, estado: estado}, ...this.pessoas];
+  adicionar(nome, idade, cidade, estado) {
+    this.pessoas = [{nome: nome, idade: idade, cidade: cidade, estado: estado}, ...this.pessoas];
   }
 
   pegarCor(idade){
