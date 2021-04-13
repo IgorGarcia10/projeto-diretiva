2679db02e1b07bf92168cd542000f384a8a5bbcb
Igor <alexandre.igor64@gmail.com>
2021-04-13 19:17:37 -0300

    Aula06

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Diretiva';
+
+  contador = 0;
+
+  contaId(contador){
+    return contador = contador+1;
+  }
+  
+  pessoas = [
+    { id: this.contador ,nome: "José", idade: 18, cidade: "São Paulo", estado: "São Paulo"},
+    { id: this.contador, nome: "Maria", idade: 23, cidade: "Rio Janeiro", estado: "Rio de janeiro" }
+  ];
+
+  textoBotao = "Esconder";
+  esconder = false;
+  alterarExibicao() {
+    this.textoBotao = this.esconder ? "Esconder" : "Exibir";
+    this.esconder = !this.esconder;
+  }
+
+  adicionar(id,nome, idade, cidade, estado) {
+    this.pessoas = [{ id: id, nome: nome, idade: idade, cidade: cidade, estado: estado}, ...this.pessoas];
+  }
+
+  pegarCor(idade){
+    return idade >=40 ? "red" : "blue";
+  }
+  
 }
